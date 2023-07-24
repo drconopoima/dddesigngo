b22371b4510c4bd80a855b06a83246e3ce0c034a
Luis Jesús Díaz Manzo <luis@drconopoima.com>
2023-07-24 15:07:21 +0200

    initial_design.go

---
+++ initial_design.go
@@ -0,0 +1,54 @@
+package initial_design
+
+import (
+	"context"
+)
+
+type SubscriptionPlan int
+
+const (
+	unknownSubscriptionPlan SubscriptionPlan = iota
+	basic
+	premium
+	exclusive
+)
+
+type AccountRole int
+
+const (
+	unknownAccountRole AccountRole = iota
+	lead
+	customer
+	churned
+	lostLead
+)
+
+type PaymentDetails struct {
+	stripeTokenID string
+}
+
+type AccountCreateForm struct {
+	AccountRole      AccountRole
+	Email            string
+	SubscriptionPlan SubscriptionPlan
+	PaymentDetails   PaymentDetails
+}
+
+type AccountModifyForm struct {
+	ID               string
+	AccountRole      AccountRole
+	Email            string
+	SubscriptionPlan SubscriptionPlan
+	PaymentDetails   PaymentDetails
+}
+
+type Account struct {
+	ID             string
+	Email          string
+	PaymentDetails PaymentDetails
+}
+
+type AccountManager interface {
+	AddAccount(ctx context.Context, form AccountCreateForm) (Account, error)
+	ModifyAccount(ctx context.Context, form AccountModifyForm) (Account, error)
+}
